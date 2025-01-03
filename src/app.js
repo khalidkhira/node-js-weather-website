9bf8c2890dbb2aa5744bef5afb04815bc556854d
danealbrain101 <danealbrain101@gmail.com>
2025-01-03 16:34:56 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require("./utils/geocode.js");
 const foreCast = require("./utils/forecast.js");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define path for express config
 const publieDirectory = path.join(__dirname, "../public");
@@ -103,6 +104,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port" + port);
 });
